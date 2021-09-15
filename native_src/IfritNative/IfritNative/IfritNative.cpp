c7c12700ec1e1943d3b219fa1b28a42da559ee33
Chloroauric Acid <70007256+Aeroraven@users.noreply.github.com>
2021-09-15 21:54:17 +0800

    Update files

--- native_src/IfritNative/IfritNative/IfritNative.cpp
+++ native_src/IfritNative/IfritNative/IfritNative.cpp
@@ -1,7 +1,10 @@
-#include "com_aeroraven_ifrit_IfritConsoleInterface.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include "com_aeroraven_ifrit_win32_IfritConsoleInterface.h"
 
 #include <iostream>
 #include <cstdio>
+#include <string>
+#include <cstring>
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -16,8 +19,18 @@
 #define IFRIT_WARN_UNSUPPORTED_OS "operating system unsupported"
 
 
+std::wstring Java_To_WStr(JNIEnv * env, jstring string){
+	std::wstring value;
+	const jchar* raw = env->GetStringChars(string, 0);
+	jsize len = env->GetStringLength(string);
+	value.assign(raw, raw + len);
+	env->ReleaseStringChars(string, raw);
+	return value;
+}
+
+
 JNIEXPORT jlong JNICALL 
-Java_com_aeroraven_ifrit_IfritConsoleInterface_ICI_1GetStdHandle
+Java_com_aeroraven_ifrit_win32_IfritConsoleInterface_ICI_1GetStdHandle
 (JNIEnv* jenv, jclass jcl, jint nStdHandle) {
 #ifdef IFRIT_WINDOWS
 	return (jlong)GetStdHandle(nStdHandle);
@@ -27,12 +40,69 @@ Java_com_aeroraven_ifrit_IfritConsoleInterface_ICI_1GetStdHandle
 }
 
 
-JNIEXPORT jboolean JNICALL
-Java_com_aeroraven_ifrit_IfritConsoleInterface_ICI_1SetConsoleTextAttribute
+JNIEXPORT jboolean JNICALL 
+Java_com_aeroraven_ifrit_win32_IfritConsoleInterface_ICI_1SetConsoleTextAttribute
 (JNIEnv* jenv, jclass jcl, jlong hConsoleOutput, jshort wAttribute) {
 #ifdef IFRIT_WINDOWS
 	return SetConsoleTextAttribute((HANDLE)hConsoleOutput, wAttribute);
 #endif
 	IFRIT_DEBUG_PROMPT(IFRIT_WARN_UNSUPPORTED_OS);
 	return (jboolean)IFRIT_INVALID_RETURN;
+}
+
+JNIEXPORT jboolean JNICALL 
+Java_com_aeroraven_ifrit_win32_IfritConsoleInterface_ICI_1SetConsoleCursorPosition
+(JNIEnv* jenv, jclass jcl, jlong hConsoleOutput, jshort wX, jshort wY) {
+#ifdef IFRIT_WINDOWS
+	COORD cCoord;
+	cCoord.X = wX;
+	cCoord.Y = wY;
+	return SetConsoleCursorPosition((HANDLE)hConsoleOutput, cCoord);
+#endif
+	IFRIT_DEBUG_PROMPT(IFRIT_WARN_UNSUPPORTED_OS);
+	return (jboolean)IFRIT_INVALID_RETURN;
+}
+
+JNIEXPORT jint JNICALL 
+Java_com_aeroraven_ifrit_win32_IfritConsoleInterface_ICI_1GetConsoleCursorPositionX
+(JNIEnv* jevnv, jclass jcl, jlong hConsoleOutput) {
+#ifdef IFRIT_WINDOWS
+	CONSOLE_SCREEN_BUFFER_INFO cinf;
+	GetConsoleScreenBufferInfo((HANDLE)hConsoleOutput, &cinf);
+	return (jint)cinf.dwCursorPosition.X;
+#endif
+	IFRIT_DEBUG_PROMPT(IFRIT_WARN_UNSUPPORTED_OS);
+	return (jint)IFRIT_INVALID_RETURN;
+}
+
+JNIEXPORT jint JNICALL
+Java_com_aeroraven_ifrit_win32_IfritConsoleInterface_ICI_1GetConsoleCursorPositionY
+(JNIEnv* jevnv, jclass jcl, jlong hConsoleOutput) {
+#ifdef IFRIT_WINDOWS
+	CONSOLE_SCREEN_BUFFER_INFO cinf;
+	GetConsoleScreenBufferInfo((HANDLE)hConsoleOutput, &cinf);
+	return (jint)cinf.dwCursorPosition.Y;
+#endif
+	IFRIT_DEBUG_PROMPT(IFRIT_WARN_UNSUPPORTED_OS);
+	return (jint)IFRIT_INVALID_RETURN;
+}
+
+
+JNIEXPORT jint JNICALL 
+Java_com_aeroraven_ifrit_win32_IfritConsoleInterface_ICI_1SetConsoleFontInfoEx
+(JNIEnv* jenv, jclass jcl, jint nFont, jshort fsx, jshort fsy, jint fontfam, jint fontweight, jstring fontface) {
+#ifdef IFRIT_WINDOWS
+	CONSOLE_FONT_INFOEX cfi;
+	cfi.cbSize = sizeof(cfi);
+	cfi.nFont = nFont;
+	cfi.dwFontSize.X = fsx;
+	cfi.dwFontSize.Y = fsy;
+	cfi.FontFamily = FF_DONTCARE;
+	cfi.FontWeight = FW_NORMAL;
+	wcscpy(cfi.FaceName, L"Arial");
+	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+	return 1;
+#endif
+	IFRIT_DEBUG_PROMPT(IFRIT_WARN_UNSUPPORTED_OS);
+	return (jint)IFRIT_INVALID_RETURN;
 }
