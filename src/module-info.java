b6bda581a9dbdd015bf9dd2cd435a8177352d426
Chloroauric Acid <70007256+Aeroraven@users.noreply.github.com>
2021-09-21 22:05:27 +0800

    Update files

--- src/module-info.java
+++ src/module-info.java
@@ -1,3 +1,16 @@
-module ifrit {
+module com.aeroraven.ifrit {
 	requires java.desktop;
+	exports com.aeroraven.ifrit.app;
+	exports com.aeroraven.ifrit.command;
+	exports com.aeroraven.ifrit.component;
+	exports com.aeroraven.ifrit.constant;
+	exports com.aeroraven.ifrit.core;
+	exports com.aeroraven.ifrit.event;
+	exports com.aeroraven.ifrit.exception;
+	exports com.aeroraven.ifrit.misc;
+	exports com.aeroraven.ifrit.primitive;
+	exports com.aeroraven.ifrit.scene;
+	exports com.aeroraven.ifrit.shape;
+	exports com.aeroraven.ifrit.parallel;
+	exports com.aeroraven.ifrit.natives;
 }
